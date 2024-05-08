06262ba7d9d9858b7bd8f5e32ebbbe92b1e9bcc8
Amkjune <ammykour76@gmail.com>
2024-05-08 10:12:02 -0400

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,42 +1,54 @@
-import random
-for z in range(0,a[pivot]):
-    pivot=a[z]
-    while(i<j):
-        for i in range(0,len(a)):
-            if a[i] < pivot:
-                continue
-            else:
-                exit
-        for j in range(len(a)-1, 0, -1):
-            if a[i] > pivot:
-                continue
-            else:
-                exit
-        v=a[i]
-        a[i]=a[j]
-        a[j]=v
-    i=a[i]
-    a[i]=pivot
-    pivot=i
-    
-print(a) 
+# Quick sort in Python
+
+# function to find the partition position
+def partition(array, low, high):
+
+  # choose the rightmost element as pivot
+  pivot = array[high]
+
+  # pointer for greater element
+  i = low - 1
+
+  # traverse through all elements
+  # compare each element with pivot
+  for j in range(low, high):
+    if array[j] <= pivot:
+      # if element smaller than pivot is found
+      # swap it with the greater element pointed by i
+      i = i + 1
+
+      # swapping element at i with element at j
+      (array[i], array[j]) = (array[j], array[i])
+
+  # swap the pivot element with the greater element specified by i
+  (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+  # return the position from where partition is done
+  return i + 1
+
+# function to perform quicksort
+def quickSort(array, low, high):
+  if low < high:
+
+    # find pivot element such that
+    # element smaller than pivot are on the left
+    # element greater than pivot are on the right
+    pi = partition(array, low, high)
+
+    # recursive call on the left of pivot
+    quickSort(array, low, pi - 1)
+
+    # recursive call on the right of pivot
+    quickSort(array, pi + 1, high)
 
 
-n = int(input("Enter number of elements : "))
- 
-# Below line read inputs from user using map() function
-a = list(map(int, 
-    input("\nEnter the numbers : ").strip().split()))[:n]
-v = 0
-print("\nList is - ", a)
-i=0
-j=len(a)-1
-pivot=0
+data = [8, 7, 2, 1, 0, 9, 6]
+print("Unsorted Array")
+print(data)
 
+size = len(data)
 
+quickSort(data, 0, size - 1)
 
 print('Sorted Array in Ascending Order:')
-print(a)
-    
-        
-
+print(data)
